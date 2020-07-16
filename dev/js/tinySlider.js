018dc6d435acd25bb2d3ad75df4724d56c856e88
Josh <josh@gdovindesigns.com>
2020-07-16 08:30:57 -0400

    made a js changee for tiny slider

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -1,62 +1,71 @@
 // console.log("tiny slider is working!");
-if ($(this).hasClass("my-slider")) {
-    tns({
-        container: '.my-slider',
-        //items: 1,
-        gutter: 20,
-        edgePadding: 50,
-        //controls: false,
-        controlsPosition: "bottom",
-        navPosition: "bottom"
-        //loop: false,
-        //startIndex: 2,
-
-        // responsive: {
-        //       "350": {
-        //         "items": 1
-        //       },
-        //       "768": {
-        //         "items": 2
-        //       },
-        //       "1440": {
-        //         "items": 3
-        //       }
-        // }
-        //axis: "vertical"
-        //"edgePadding": 50
-        //slideBy: 'page',
-        //autoplay: true
-    });
 
 
-    tns({
-        container: '.my-slider-2',
-        //items: 1,
-        gutter: 20,
-        edgePadding: 50,
-        //controls: false,
-        controlsPosition: "bottom",
-        navPosition: "bottom"
-        //loop: false,
-        //startIndex: 2,
+$(document).ready(function () {
 
-        // responsive: {
-        //       "350": {
-        //         "items": 1
-        //       },
-        //       "768": {
-        //         "items": 2
-        //       },
-        //       "1440": {
-        //         "items": 3
-        //       }
-        // }
-        //axis: "vertical"
-        //"edgePadding": 50
-        //slideBy: 'page',
-        //autoplay: true
-    });
+    console.log($(".slides").hasClass("my-slider"));
+    if ($(".slides").hasClass("my-slider")) {
+        tns({
+            container: '.my-slider',
+            //items: 1,
+            gutter: 20,
+            edgePadding: 50,
+            //controls: false,
+            controlsPosition: "bottom",
+            navPosition: "bottom"
+            //loop: false,
+            //startIndex: 2,
 
-} else{
-    console.log("No TinySlider found!");
-}
+            // responsive: {
+            //       "350": {
+            //         "items": 1
+            //       },
+            //       "768": {
+            //         "items": 2
+            //       },
+            //       "1440": {
+            //         "items": 3
+            //       }
+            // }
+            //axis: "vertical"
+            //"edgePadding": 50
+            //slideBy: 'page',
+            //autoplay: true
+        });
+
+    }
+
+    if ($(".slides").hasClass("my-slider-2")) {
+        tns({
+            container: '.my-slider-2',
+            //items: 1,
+            gutter: 20,
+            edgePadding: 50,
+            //controls: false,
+            controlsPosition: "bottom",
+            navPosition: "bottom"
+            //loop: false,
+            //startIndex: 2,
+
+            // responsive: {
+            //       "350": {
+            //         "items": 1
+            //       },
+            //       "768": {
+            //         "items": 2
+            //       },
+            //       "1440": {
+            //         "items": 3
+            //       }
+            // }
+            //axis: "vertical"
+            //"edgePadding": 50
+            //slideBy: 'page',
+            //autoplay: true
+        });
+
+    } else {
+        console.log("No TinySlider found!");
+    }
+
+});
