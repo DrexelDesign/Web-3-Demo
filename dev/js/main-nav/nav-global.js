5a9022b6f9416c65afedb065ca8e26ab25a38197
Josh <josh@gdovindesigns.com>
2020-07-22 10:18:34 -0400

    added animated burger

--- dev/js/main-nav/nav-global.js
+++ dev/js/main-nav/nav-global.js
@@ -1,7 +1,36 @@
 
+var canYouSeeTheMenu = false;
+
+
+
+
+
+
+
 // function to handle the showing and hiding of the main-nav
 function hideShowMainNav(){
-    console.log("hide or show nav");
+    //console.log("hide or show nav");
     //toggles the css display property
-    $("#main-nav").toggle();
+   // $("#main-nav").toggle();
+
+    if(canYouSeeTheMenu === false){
+        console.log("show me the menu!");
+        // reset back to true, so that the menu will go away
+        canYouSeeTheMenu = true;
+
+        //turn the burger into an X
+        animateBurger();
+
+
+
+    }else{
+        console.log("hide the menu!");
+        // reset back to false, so the you can bring the menu back!
+        canYouSeeTheMenu = false;
+        
+        //turn the X into the burger
+        animateBurger();
+    }
+
+
 }
