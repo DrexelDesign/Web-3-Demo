a5983fbb3bc9b9ef2b031b6dab40e5ad21d2d25a
Josh <josh@gdovindesigns.com>
2020-08-03 09:59:02 -0400

    updated nav

--- dev/js/main-nav/burger-mouse-events.js
+++ dev/js/main-nav/burger-mouse-events.js
@@ -1,46 +1,27 @@
 
-var burgerToArrowTimeline = gsap.timeline({paused:true});
-var burgerArrowSpeed = 0.25;
-
-
-burgerToArrowTimeline.to("#burger",{duration:burgerArrowSpeed, rotation:-90}, "animateBurger")
-                        .to("#bottom-line",{duration:burgerArrowSpeed, y:-10}, "animateBurger")
-                        .to("#top-line",{duration:burgerArrowSpeed, y:10}, "animateBurger")
-                        .to("#down-left-arrow",{duration:burgerArrowSpeed, rotation:55}, "createArrow")
-                        .to("#down-right-arrow",{duration:burgerArrowSpeed, rotation:-55}, "createArrow");
-
+//var burgerToArrowTimeline = gsap.timeline({paused:true});
 
 gsap.set("#up-left-arrow",{transformOrigin:"right center", alpha:0});
 gsap.set("#up-right-arrow",{transformOrigin:"right center", alpha:0});
 
-var xToUpArrowTimeline = gsap.timeline({paused:true});          
-xToUpArrowTimeline.to("#top-line",{duration:burgerArrowSpeed, rotation:0},"createArrowUp")
-                    .to("#bottom-line",{duration:burgerArrowSpeed, rotation:0},"createArrowUp")
-                    .to("#up-left-arrow",{duration:burgerArrowSpeed, rotation:55, alpha:1},"createArrowUp")
-                    .to("#up-right-arrow",{duration:burgerArrowSpeed, rotation:-55, alpha:1},"createArrowUp")
 
 
 $("#burger").on("mouseenter", function(){
     //console.log("mouse enter");
-
     //console.log(canYouSeeTheMenu + " menu visible");
     if(canYouSeeTheMenu === false){
-        burgerToArrowTimeline.play();
+        burgerAnimationTimeLine.play("burgerToDownArrow");
     }else{
-        xToUpArrowTimeline.play()
+        burgerAnimationTimeLine.play("XtoUpArrow");
     }
 })
 
 
-
 $("#burger").on("mouseleave", function(){
-    //console.log("mouse leave");
-    //console.log(canYouSeeTheMenu + " menu visible");
-
     if(canYouSeeTheMenu === false){
-        burgerToArrowTimeline.reverse();
+        burgerAnimationTimeLine.reverse("burgerToDownArrowReverse");
+        
     }else{
-        xToUpArrowTimeline.reverse()
+        burgerAnimationTimeLine.reverse("XtoUpArrowReverse");
     }
-    
 })
